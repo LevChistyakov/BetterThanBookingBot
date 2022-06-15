df6419a4379d889a2b9077f642edffe51053e3ed
Lev <leoncameron3131@gmail.com>
2022-06-15 15:23:36 +0300

    Changed main.py to aiogram syntax

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from loader import bot
+from aiogram import executor
+
+from loader import dp
 import handlers
 from utils.set_bot_commands import set_default_commands
 
+
+async def on_startup(dispatcher):
+    await set_default_commands(dispatcher)
+
+
 if __name__ == '__main__':
-    set_default_commands(bot)
-    bot.infinity_polling()
+    executor.start_polling(dp, on_startup=on_startup)
